4dfbf07ae5c466ce2f732ff52abc52c505e9b1e5
Deyvid Arauz <deyvida1001@gmail.com>
2022-05-03 16:17:58 -0600

    Initial config install react

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  output: {
+    filename: "app.bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
