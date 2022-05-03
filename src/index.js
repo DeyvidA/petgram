2c750201b5fcfe27982496f9ff2dd3e4c87c48b8
Deyvid Arauz <deyvida1001@gmail.com>
2022-05-03 16:35:06 -0600

    install react and babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("We started the app");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("app"));
+root.render(<h1>Seguimos con el curso avanzado de react</h1>);
